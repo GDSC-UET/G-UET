21996fc17ac806ceaf20c1d82ea55bb163234ddb
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-08-13 23:31:17 +0700

    feat: homepage banner

---
+++ src/components/home/banner.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex w-full items-center justify-center text-center lg:justify-between lg:text-left">
+    <div class="flex flex-col items-center gap-16 lg:ml-16 lg:items-start">
+      <h1 class="max-w-[500px] text-6xl font-bold">
+        <span class="text-primary">Preparation</span> for your
+        <span class="text-primary">career</span>
+      </h1>
+      <CommonSearch />
+    </div>
+    <NuxtImg src="/lead.png" class="hidden w-[600px] lg:block" />
+  </div>
+</template>
