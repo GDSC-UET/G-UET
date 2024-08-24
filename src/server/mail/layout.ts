d1a3912f7a3b2f22ae544011ab9796cb823f87cc
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-08-24 22:50:25 +0700

    feat: mail layout

---
+++ src/server/mail/layout.ts
@@ -0,0 +1,50 @@
+export const MailLayout = `<div style="border: 1px solid #3b82f6; width: 600px; font-family: sans-serif">
+      <div
+        style="
+          border-bottom: 1px solid #3b82f6;
+          width: 100%;
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 120px;
+        "
+      >
+        <img
+          src="https://res.cloudinary.com/dszkt92jr/image/upload/v1724506930/logo_uno9g7.png"
+          alt="Logo"
+          style="width: 500px"
+        />
+      </div>
+      <div
+        style="
+          border-top: 1px solid #3b82f6;
+          width: 540px;
+          height: 200px;
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          justify-content: center;
+          padding: 0 30px;
+          gap: 28px;
+          background: #F1F5F9;
+        "
+      >
+        <img
+          src="https://res.cloudinary.com/dszkt92jr/image/upload/v1724506930/logo_uno9g7.png"
+          alt="Logo"
+          style="width: 350px"
+        />
+        <p style="font-size: 12px; display: flex; flex-direction: column; gap: 4px;">
+          <span>© 2024 Google Developer Student Clubs - UET </span
+          <span>144 Xuan Thuy, Dich Vong Hau, Cau Giay, Ha Noi</span>
+        </p>
+        <a href="https://facebook.com">
+          <img
+            src="https://res.cloudinary.com/dszkt92jr/image/upload/v1720703779/Social_Icon_Link_ibwiog.png"
+            alt="Fanpage"
+            style="width: 20px"
+          />
+        </a>
+      </div>
+    </div>`
