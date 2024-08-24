09cc15a9cb6f9b55247bb5d66ef31b3bfe3fc32e
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-08-25 02:50:27 +0700

    feat: no members found

---
+++ src/pages/members/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <UContainer class="mt-8 flex flex-col items-center gap-12">
+    <CommonTitleText prefix="Our" text="members" />
+    <div class="flex flex-col items-center gap-4">
+      <CommonSearch />
+      <div class="flex gap-4">
+        <USelect v-model="teamSelect" :options="teams" size="lg" />
+        <USelect v-model="genSelect" :options="gens" size="lg" />
+      </div>
+    </div>
+    <MembersNoMembersFound />
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+const teams = ref([
+  'All Teams',
+  'Technical',
+  'Event',
+  'Human Resources',
+  'Marketing',
+  'Alumni',
+  'Board of Leader'
+])
+
+const gens = ref(['All Gens', 'Gen 0', 'Gen 1'])
+
+const teamSelect = ref([teams.value[0]])
+const genSelect = ref([gens.value[0]])
+</script>
